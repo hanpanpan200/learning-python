85e2766ae4720cb541ad14c6a4868781c9f0bd23
Panpan Han <pphan@thoughtworks.com>
2018-08-04 15:28:32 +0800

    feat: init repo

---
+++ string-demo.py
@@ -0,0 +1,21 @@
+line_break_str = '''Hello World!
+This is a line break demo!'''
+print(line_break_str)
+
+special_quote_str = '''He said, "Aren't can't shouldn't wouldn't"'''
+print(special_quote_str)
+
+replacement_str1 = 'I scored %s points in the exam!'
+print(replacement_str1 % 100)
+
+replacement_str2 = 'I scored %s points for English and %s points for Math.'
+print(replacement_str2 % (100, 90))
+
+process_str = 's'
+print(5 * process_str)
+
+
+
+
+
+
