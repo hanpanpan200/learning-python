9822a080d5537532871a7d0f539ba3f8bbc802f6
Grace Han <hanpanpan200@gmail.com>
2018-08-06 17:45:53 +0800

    feat: add exercise demo code

---
+++ exercise_3.py
@@ -0,0 +1,21 @@
+# 1
+games = ['Basketball', 'Baseball', 'Football']
+foods = ['Banana', 'Apple', 'Juice']
+favorites = games + foods
+print(favorites)
+
+# 2
+building_number = 3
+ninja_number_per_building = 25
+tunnel_number_per_building = 2
+warrior_number_per_tunnel = 40
+total_ninja_number = ninja_number_per_building * building_number
+total_warrior_number = warrior_number_per_tunnel * tunnel_number_per_building * building_number
+print('Total ninja number is %s' % total_ninja_number)
+print('Total warrior number is %s' % total_warrior_number)
+
+# 3
+#coding:utf-8
+first_name = 'Panpan'
+last_name = 'Han'
+print('你好， %s %s' % (first_name, last_name))
